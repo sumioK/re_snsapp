0726ef53be5f438041f7f9b8cfc99889da3b2f25
sumiok <tap.miwo@gmail.com>
2023-02-23 14:05:07 +0900

    success to signup

--- boardapp/views.py
+++ boardapp/views.py
@@ -1,3 +1,15 @@
+from django.contrib.auth.models import User
+from django.db import IntegrityError
 from django.shortcuts import render
 
-# Create your views here.
+def signupfunc(request):
+  if request.method == "POST":
+    username = request.POST['username']
+    password = request.POST['password']
+    try:
+      user = User.objects.create_user(username, '', password)
+      return render(request, 'signup.html',{})
+
+    except IntegrityError:
+      return render(request, 'signup.html',{'error': 'このユーザー名は既に使用されています'})
+  return render(request, 'signup.html',{})
